1a065b22523a7fad98d3ec72ef5e0b08902234f2
ChadwickZackery <94324421+SpencerNold@users.noreply.github.com>
2025-08-25 23:57:11 -0700

    Basic Example
    Wrote a basic example for KWAF.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,14 @@ plugins {
 	kotlin("jvm") version "1.9.20"
 }
 
-repositories {
-	mavenCentral()
-}
-
 kotlin {
 	jvmToolchain {
 		languageVersion.set(JavaLanguageVersion.of(8))
 	}
 }
 
-tasks.withType<KotlinCompile>().configureEach {
-	compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+repositories {
+	mavenCentral()
 }
 
 dependencies {
@@ -24,6 +20,19 @@ dependencies {
 
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+sourceSets {
+	create("examples") {
+		kotlin.srcDir("src/examples/kotlin")
+		resources.srcDir("src/examples/resources")
+		compileClasspath += sourceSets["main"].output + configurations["testRuntimeClasspath"]
+		runtimeClasspath += output + compileClasspath
+	}
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+	compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
 }
 
 tasks.test {
---
+++ src/examples/kotlin/BasicControllerExample.kt
@@ -0,0 +1,23 @@
+import me.spencernold.kwaf.Protocol
+import me.spencernold.kwaf.Resource
+import me.spencernold.kwaf.Route
+import me.spencernold.kwaf.WebServer
+import me.spencernold.kwaf.services.Service
+import java.io.InputStream
+import java.util.concurrent.Executors
+
+@Service.Controller
+class BasicController {
+    @Route.File(path = "/")
+    fun root(): InputStream? {
+        return Resource.get("basic_example.html")
+    }
+}
+
+fun main() {
+    val server = WebServer.Builder(Protocol.HTTP, 80)
+        .executor(Executors.newCachedThreadPool())
+        .services(BasicController::class.java)
+        .build()
+    server.start()
+}

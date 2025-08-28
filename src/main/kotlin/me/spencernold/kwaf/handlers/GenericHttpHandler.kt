6a2f94de6a908378bf53294cb508603da4db5120
ChadwickZackery <94324421+SpencerNold@users.noreply.github.com>
2025-08-27 17:32:28 -0700

    Update GenericHttpHandler.kt
    Rewrote the handle function of the GenericHttpHandler to make it more readable.

--- src/main/kotlin/me/spencernold/kwaf/handlers/GenericHttpHandler.kt
+++ src/main/kotlin/me/spencernold/kwaf/handlers/GenericHttpHandler.kt
@@ -1,7 +1,5 @@
 package me.spencernold.kwaf.handlers
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
 import me.spencernold.kwaf.Route
@@ -10,57 +8,59 @@ import me.spencernold.kwaf.http.HttpRequest
 import me.spencernold.kwaf.http.HttpResponse
 import me.spencernold.kwaf.util.InputStreams
 import java.lang.reflect.Method
-import java.nio.charset.StandardCharsets
 
 class GenericHttpHandler(private val route: Route, private val instance: Any, private val method: Method): Handler(), HttpHandler {
 
     override fun handle(exchange: HttpExchange) {
-        val encoder = route.encoding.getEncoder()
         if (exchange.requestMethod == route.method.name) {
+            // Request Handling
             val body = InputStreams.readAllBytes(exchange.requestBody)
             if (body.isEmpty() == route.input) {
                 exchange.sendResponseHeaders(400, -1)
                 return
             }
-            if (route.input) {
-                if (method.parameterCount == 0)
-                    throw HandlerException("@Route is expecting input, but no parameters to the ${method.name} are given")
-            } else {
-                if (method.parameterCount != 0)
-                    throw HandlerException("@Route is not expecting input, but parameters to the ${method.name} exist")
-            }
-            method.isAccessible = true
-            val response: Any? = if (route.input) {
-                val parameter = method.parameters[0]
-                val argument: Any? = if (parameter.type == HttpRequest::class.java) {
-                    val uri = exchange.requestURI.toASCIIString().split("?")
-                    val headers = mutableMapOf<String, String>()
-                    for (key in exchange.requestHeaders.keys)
-                        headers[key] = exchange.requestHeaders.getValue(key)[0]
-                    val parameters = mutableMapOf<String, String>()
-                    for (str in uri[1].split("&")) {
-                        val s = str.split("=")
-                        parameters[s[0]] = s[1]
-                    }
-                    HttpRequest(route.method, uri[0], parameters, headers, body)
-                } else {
-                    if (encoder == null) String(body) else encoder.decode(String(body), parameter.type)
+            val encoder = route.encoding.getEncoder()
+            val response: Any?
+            val param0 = method.parameters[0]
+            if (param0.type == HttpRequest::class.java && method.parameters.size == 1) {
+                val uri = exchange.requestURI
+                val parameters = mutableMapOf<String, String>()
+                val values = uri.query.split("&")
+                for (s in values) {
+                    val pair = s.split("=")
+                    parameters[pair[0]] = pair[1]
                 }
-                method.invoke(instance, argument)
+                val headers = mutableMapOf<String, String>()
+                for (key in exchange.requestHeaders.keys)
+                    headers[key] = exchange.requestHeaders.getValue(key)[0]
+                val request = HttpRequest(route.method, uri.path, parameters, headers, body)
+                response = method.invoke(instance, request)
+            } else if (route.input && method.parameters.size == 1) {
+                response = encoder?.decode(String(body), param0.type) ?: String(body)
             } else {
-                method.invoke(instance)
+                if (method.parameters.isNotEmpty())
+                    throw HandlerException("@Route is not expecting input, but parameters to the ${method.name} exist")
+                response = method.invoke(instance)
             }
+            // Response Handling
             if (response == null) {
                 exchange.sendResponseHeaders(204, -1)
                 return
             }
-            val bytes = if (response is HttpResponse) { response.body } else { (encoder?.encode(response)?.toByteArray() ?: response.toString().toByteArray()) }
-            if (bytes.isEmpty()) {
-                exchange.sendResponseHeaders(if (response is HttpResponse) { response.code } else { 204 }, -1)
-                return
+            if (response is HttpResponse) {
+                val bytes = response.body
+                exchange.sendResponseHeaders(response.code, if (bytes.isEmpty() && response.code == 204) -1 else bytes.size.toLong())
+                if (bytes.isNotEmpty())
+                    exchange.responseBody.write(bytes)
+            } else {
+                val bytes = (encoder?.encode(response)?.toByteArray() ?: response.toString().toByteArray())
+                if (bytes.isEmpty())
+                    exchange.sendResponseHeaders(204, -1)
+                else {
+                    exchange.sendResponseHeaders(200, bytes.size.toLong())
+                    exchange.responseBody.write(bytes)
+                }
             }
-            exchange.sendResponseHeaders(if (response is HttpResponse) { response.code } else { 200 }, bytes.size.toLong())
-            exchange.responseBody.write(bytes)
         } else {
             exchange.sendResponseHeaders(400, -1)
         }
